2186d48ea27bf4395a457be84ac94696a5596282
disection <diseccionmexico@gmail.com>
2022-08-29 10:00:46 -0500

    add-basic-estructure-html

--- src/App.js
+++ src/App.js
@@ -2,7 +2,40 @@
 
 function App() {
   return (
-    <h1>Rick and Morty APP</h1>
+    <>
+      <section className="detail">
+        <div>
+          <article><div className="img">
+            <img src="https://rickandmortyapi.com/api/character/avatar/162.jpeg" alt="Ice-T" />
+          </div>
+            <div className="content">
+              <h1>Ice-T</h1>
+              <h2>Alive - Alien</h2>
+              <h3>Last known location:</h3>
+              <p>Alphabetrium</p>
+              <h3>First seen in:</h3>
+              <p>Get Schwifty</p>
+            </div>
+          </article>
+        </div>
+      </section>
+      <section>
+        <header><h1></h1></header>
+        <main>
+          <article>
+            <div className="img">
+              <img src="https://rickandmortyapi.com/api/character/avatar/162.jpeg" alt="Ice-T" />
+            </div>
+            <div className="content">
+              <h1>Ice-T <span>Alive - Alien</span></h1>      
+              
+            </div>
+          </article>
+        </main>
+        <footer>CHARACTERS: 826  LOCATIONS: 126 EPISODES: 51</footer></section>
+
+    </>
+
   );
 }
 
